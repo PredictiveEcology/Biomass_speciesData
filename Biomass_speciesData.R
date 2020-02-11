0da6fa9a851390b7b418f2b37361c7394d414079
Alex Chubaty <alex.chubaty@gmail.com>
2020-02-11 12:47:34 -0700

    workaround for raster extent

--- Biomass_speciesData.R
+++ Biomass_speciesData.R
@@ -172,6 +172,10 @@ biomassDataInit <- function(sim) {
   origFilenames <- vapply(layerNames(sim$speciesLayers), function(r) filename(sim$speciesLayers[[r]]),
                           character(1))
 
+  ## TODO: remove workaround for extent mismatches
+  if (extent(sim$speciesLayers) < extent(sim$rasterToMatchLarge))
+    sim$speciesLayers <- raster::extend(sim$speciesLayers, sim$rasterToMatchLarge)
+
   ## re-enforce study area mask (merged/summed layers are losing the mask)
   sim$speciesLayers <- raster::mask(sim$speciesLayers, sim$rasterToMatchLarge)
 
