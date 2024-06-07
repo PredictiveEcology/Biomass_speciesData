d87a9b09f9f52b285a111d18db4eec908519013a
Ceres Barros <ceres.barros@nrcan-rncan.gc.ca>
2024-06-06 17:08:04 -0700

    bump v1.0.4

--- Biomass_speciesData.R
+++ Biomass_speciesData.R
@@ -13,7 +13,7 @@ defineModule(sim, list(
     person(c("Alex", "M."), "Chubaty", email = "achubaty@for-cast.ca", role = c("aut"))
   ),
   childModules = character(0),
-  version = list(Biomass_speciesData = "1.0.3.9001"),
+  version = list(Biomass_speciesData = "1.0.4"),
   spatialExtent = raster::extent(rep(NA_real_, 4)),
   timeframe = as.POSIXlt(c(NA, NA)),
   timeunit = "year",
